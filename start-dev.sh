573094f8d5bbe78a84d6e9da5c19c815cd4f20e4
Ruben Alvarez <info@rubenalvarez.dev>
2025-01-21 07:55:45 +0100

    Simplify to absolute basics

--- start-dev.sh
+++ start-dev.sh
@@ -4,29 +4,10 @@ echo "Starting development environment..."
 
 # Wait for MySQL
 echo "Waiting for MySQL to be ready..."
-
-# Try to resolve MySQL IP
-echo "Resolving MySQL IP..."
-MYSQL_IP=$(getent hosts daw_db | awk '{ print $1 }')
-
-if [ -z "$MYSQL_IP" ]; then
-    echo "Could not resolve MySQL IP. Using container name..."
-    MYSQL_HOST="daw_db"
-else
-    echo "MySQL IP resolved: $MYSQL_IP"
-    MYSQL_HOST="$MYSQL_IP"
-fi
-
-until mysql -h"$MYSQL_HOST" -u"root" -p"root" -e "SELECT 1" > /dev/null 2>&1; do
-    echo "MySQL is unavailable - sleeping"
-    sleep 1
-done
-
-echo "MySQL is up and running!"
+sleep 30
 
 # Setup Laravel
 cd /var/www/html
-echo "DB_HOST=$MYSQL_HOST" >> .env.docker
 cp .env.docker .env
 php artisan migrate --force
 
